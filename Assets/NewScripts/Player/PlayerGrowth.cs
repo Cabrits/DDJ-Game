daae9439fe402b166aedd9b94e474391da1d8c70
Armando Brandão <armandobrandao192@gmail.com>
2024-01-17 18:35:50 +0000

    Update Player Growth and Lv2

--- Assets/NewScripts/Player/PlayerGrowth.cs
+++ Assets/NewScripts/Player/PlayerGrowth.cs
@@ -6,11 +6,11 @@ public class PlayerGrowth : MonoBehaviour{
 
 
     public void Grow(GameObject target){
-        target.transform.localScale += new Vector3(0.2f, 0.2f);
+        target.transform.localScale += new Vector3(0.3f, 0.3f);
     }
 
     public void Shrink(GameObject target){
-       target.transform.localScale -= new Vector3(0.2f, 0.2f);
+       target.transform.localScale -= new Vector3(0.3f, 0.3f);
     }
 
 }
