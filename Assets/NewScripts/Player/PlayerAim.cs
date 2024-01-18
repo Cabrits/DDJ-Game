2de54c24940eddabe94f3741393951aeec376814
Armando Brandão <armandobrandao192@gmail.com>
2024-01-18 12:44:43 +0000

    Player Fix

--- Assets/NewScripts/Player/PlayerAim.cs
+++ Assets/NewScripts/Player/PlayerAim.cs
@@ -15,23 +15,8 @@ public class PlayerAim : MonoBehaviour{
 
     void FixedUpdate(){
         Vector2 lookDirection = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f; //Mathf.Atan2 calcula o angulo em radianos - Mathf.Rad2Deg converte de radianos para graus
-        //print(angle);
-    /*
-        if (angle < 0 && angle > -90){
-            angle = 0;
-        }
-        else if (angle < -90 && angle > -180){
-            angle = -180;
-        }
-        else if (angle > 0 && angle < 90){
-            angle = 0;
-        }
-        else if (angle > 90 && angle < 180){
-            angle = 180;
-        }
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg; //Mathf.Atan2 calcula o angulo em radianos - Mathf.Rad2Deg converte de radianos para graus
         rb.rotation = angle;
-    */
     }
     
 }
